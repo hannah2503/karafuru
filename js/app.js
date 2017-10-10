3a57aba621268a7e68a1c17471903c70008f254d
crossy87 <hannabmcross@gmail.com>
2017-10-10 16:31:32 +0100

    working game logic

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ $(() => {
   let $userArray = [];
   let $squareSequence = [];
   let delay = 0;
-  let level = 3;
+  const level = 3;
   let score = 0;
-  let $score = $('.score');
+  const $score = $('.score');
   const $matchStatus = $('.matchStatus');
   const $square = $('.square');
   const $lis = $('li');
-  const $playButton = $('button');
+  const $button = $('button');
   let colors = [ '#9C89B8', '#F0A6CA','#EFC3E6', '#F0E6EF', '#B8BEDD'];
   const originColor = '#FE938C';
   let randomColor = [];
@@ -24,7 +24,7 @@ $(() => {
   function computerPlay() {
     console.log(`computerplay im hit --> ${$squareSequence.length} `);
     //random color generator
-    for (var j = 0; j < 3; j++) {
+    for (var j = 0; j < level; j++) {
       const newRandom = Math.floor(Math.random() * colors.length);
       randomColor.push(colors[newRandom]);
       colors.splice(newRandom, 1);
@@ -69,66 +69,61 @@ $(() => {
         console.log('keep clicking!');
       }
     });
-    //function which compares user array with computer array
-    function comparison(){
-      const arr1 = $userArray.toString();
-      const arr2 = $squareSequence.toString();
-      if (arr1 === arr2){
-        console.log('match!');
-        $matchStatus.text('It\'s a match!');
-        $score.text(`${score += 1}`);
+  }
+  //function which compares user array with computer array
+  function comparison(){
+    const arr1 = $userArray.toString();
+    const arr2 = $squareSequence.toString();
+    if (arr1 === arr2){
+      console.log('match!');
+      $matchStatus.text('It\'s a match!');
+      $score.text(`${score += 1}`);
+      setTimeout(() => {
+        reset();
+        shuffle();
         setTimeout(() => {
-          reset();
-          shuffle();
-          setTimeout(() => {
-            playAgain();
-          }, 1000);
+          playAgain();
         }, 1000);
-      } else {
-        console.log('not a match');
-        $matchStatus.text('Game Over');
-        setTimeout(() => {
-          gameReset();
-          shuffle();
-        }, 1000);
-      }
+      }, 1000);
+    } else {
+      console.log('not a match');
+      $matchStatus.text('Game Over');
+      setTimeout(() => {
+        gameReset();
+        shuffle();
+      }, 1000);
     }
-    //function to start game
-    $playButton.on('click', function() {
-      console.log('you clicked play');
-      computerPlay();
-      console.log('computer is playing');
-      clearDisplay();
-      console.log('board is clearing');
-      $playButton.css('display', 'none');
-    });
+  }
 
-    //function to call next sequence in game
-    function playAgain(){
-      $square.off('click');
-      computerPlay();
-      clearDisplay();
-    }
 
-    //reset for when play continues
-    function reset (){
-      $matchStatus.text('');
-      delay = 0;
-      $userArray =[];
-      $squareSequence =[];
-      randomColor = [];
-    }
+  //function to start game
+  $button.on('click', function() {
+    computerPlay();
+    clearDisplay();
+  });
 
-    //reset for when it's game over
-    function gameReset(){
-      $score.text('0');
-      score = 0;
-      delay = 0;
-      $userArray =[];
-      $squareSequence =[];
-      randomColor=[];
-      $playButton.css('display', 'block');
-    }
+  //function to call next sequence in game
+  function playAgain(){
+    $square.off('click');
+    computerPlay();
+    clearDisplay();
+  }
 
+  //reset for when play continues
+  function reset (){
+    $matchStatus.text('');
+    delay = 0;
+    $userArray =[];
+    $squareSequence =[];
+    randomColor = [];
+  }
+  //reset for when it's game over
+  function gameReset(){
+    $score.text('0');
+    score = 0;
+    delay = 0;
+    $userArray =[];
+    $squareSequence =[];
+    randomColor=[];
   }
 });
